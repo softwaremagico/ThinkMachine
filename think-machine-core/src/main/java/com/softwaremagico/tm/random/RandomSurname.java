d06a4e1d9ba6dea8b9dd2d4a3e187596aade6bfa
Jorge Hortelano <softwaremagico@gmail.com>
2018-09-09 21:31:09 +0200

    no nobility cannot have nobility surnames

--- think-machine-core/src/main/java/com/softwaremagico/tm/random/RandomSurname.java
+++ think-machine-core/src/main/java/com/softwaremagico/tm/random/RandomSurname.java
@@ -1,14 +1,40 @@
 package com.softwaremagico.tm.random;
 
+/*-
+ * #%L
+ * Think Machine (Core)
+ * %%
+ * Copyright (C) 2017 - 2018 Softwaremagico
+ * %%
+ * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
+ * <softwaremagico@gmail.com> Valencia (Spain).
+ *  
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import java.util.Set;
 import java.util.TreeMap;
 
 import com.softwaremagico.tm.InvalidXmlElementException;
 import com.softwaremagico.tm.character.CharacterPlayer;
 import com.softwaremagico.tm.character.Surname;
+import com.softwaremagico.tm.character.factions.Faction;
 import com.softwaremagico.tm.character.factions.FactionGroup;
 import com.softwaremagico.tm.character.factions.FactionsFactory;
 import com.softwaremagico.tm.character.race.InvalidRaceException;
+import com.softwaremagico.tm.log.RandomGenerationLog;
 import com.softwaremagico.tm.random.exceptions.InvalidRandomElementSelectedException;
 import com.softwaremagico.tm.random.selectors.IRandomPreferences;
 
@@ -46,6 +72,16 @@ public class RandomSurname extends RandomSelector<Surname> {
 			} else {
 				return 0;
 			}
+		}
+		// Not nobility no faction as surname.
+		try {
+			for (Faction faction : FactionsFactory.getInstance().getElements(getCharacterPlayer().getLanguage())) {
+				if (faction.getName().contains(surname.getName())) {
+					return 0;
+				}
+			}
+		} catch (InvalidXmlElementException e) {
+			RandomGenerationLog.errorMessage(this.getClass().getName(), e);
 		}
 		// Not nobility, use surnames of the planet.
 		if (getCharacterPlayer().getInfo().getPlanet() != null && !getCharacterPlayer().getInfo().getPlanet().getSurnames().isEmpty()) {
