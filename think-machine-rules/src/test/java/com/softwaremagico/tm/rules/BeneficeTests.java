cc32ba4a785f6c152a6279fd944405f429c1785a
Software Mágico <softwaremagico@gmail.com>
2021-03-01 12:49:35 +0100

    Created test for checking alba minor house restrictions.

--- think-machine-rules/src/test/java/com/softwaremagico/tm/rules/BeneficeTests.java
+++ think-machine-rules/src/test/java/com/softwaremagico/tm/rules/BeneficeTests.java
@@ -24,19 +24,14 @@ package com.softwaremagico.tm.rules;
  * #L%
  */
 
-import org.testng.annotations.Test;
-
 import com.softwaremagico.tm.InvalidXmlElementException;
 import com.softwaremagico.tm.character.CharacterPlayer;
 import com.softwaremagico.tm.character.benefices.AvailableBeneficeFactory;
 import com.softwaremagico.tm.character.benefices.BeneficeAlreadyAddedException;
 import com.softwaremagico.tm.character.benefices.InvalidBeneficeException;
-import com.softwaremagico.tm.character.blessings.BlessingAlreadyAddedException;
-import com.softwaremagico.tm.character.blessings.TooManyBlessingsException;
-import com.softwaremagico.tm.character.cybernetics.RequiredCyberneticDevicesException;
-import com.softwaremagico.tm.character.cybernetics.TooManyCyberneticDevicesException;
 import com.softwaremagico.tm.character.factions.FactionsFactory;
 import com.softwaremagico.tm.file.PathManager;
+import org.testng.annotations.Test;
 
 @Test(groups = {"benefices"})
 public class BeneficeTests {
@@ -50,5 +45,21 @@ public class BeneficeTests {
                 FactionsFactory.getInstance().getElement("amaltheans", LANGUAGE, PathManager.DEFAULT_MODULE_FOLDER));
         player.addBenefice(AvailableBeneficeFactory.getInstance().getElement("cash [firebirds3000]", LANGUAGE,
                 PathManager.DEFAULT_MODULE_FOLDER));
+    }
+
+    @Test
+    public void checkRNobilityBeneficesMinorHouses() throws InvalidXmlElementException, BeneficeAlreadyAddedException {
+        final CharacterPlayer player = new CharacterPlayer(LANGUAGE, PathManager.DEFAULT_MODULE_FOLDER);
+        player.setFaction(FactionsFactory.getInstance().getElement("alba", LANGUAGE,
+                PathManager.DEFAULT_MODULE_FOLDER));
+        player.addBenefice(AvailableBeneficeFactory.getInstance().getElement("nobility [knight]", LANGUAGE, PathManager.DEFAULT_MODULE_FOLDER));
+    }
+
+    @Test(expectedExceptions = InvalidBeneficeException.class)
+    public void checkRestrictedBeneficesMinorHouses() throws InvalidXmlElementException, BeneficeAlreadyAddedException {
+        final CharacterPlayer player = new CharacterPlayer(LANGUAGE, PathManager.DEFAULT_MODULE_FOLDER);
+        player.setFaction(FactionsFactory.getInstance().getElement("alba", LANGUAGE,
+                PathManager.DEFAULT_MODULE_FOLDER));
+        player.addBenefice(AvailableBeneficeFactory.getInstance().getElement("nobility [earl]", LANGUAGE, PathManager.DEFAULT_MODULE_FOLDER));
     }
 }
