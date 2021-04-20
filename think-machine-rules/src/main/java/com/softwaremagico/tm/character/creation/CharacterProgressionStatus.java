28dab217b7b43f35b40fcb67499fa20502cb0024
Software Mágico <softwaremagico@gmail.com>
2021-04-20 13:23:00 +0200

    Sorted character status

--- think-machine-rules/src/main/java/com/softwaremagico/tm/character/creation/CharacterProgressionStatus.java
+++ think-machine-rules/src/main/java/com/softwaremagico/tm/character/creation/CharacterProgressionStatus.java
@@ -8,17 +8,17 @@ package com.softwaremagico.tm.character.creation;
  * %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> Valencia (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -26,11 +26,11 @@ package com.softwaremagico.tm.character.creation;
 
 public enum CharacterProgressionStatus {
     INVALID,
-    DRAFT,
-    EQUIPPED,
-    EXTENDED,
-    FINISHED,
-    IN_PROGRESS,
+    UNDEFINED,
     NOT_STARTED,
-    UNDEFINED;
+    DRAFT,
+    IN_PROGRESS,
+    EQUIPPED,
+    FINISHED,
+    EXTENDED,
 }
