5558f6391cc45536154123b0dd15f9c5644e2ae5
softwaremagico <softwaremagico@gmail.com>
2022-08-14 07:24:29 +0200

    Closing resource

--- think-machine-pdf/src/main/java/com/softwaremagico/tm/pdf/complete/PdfDocument.java
+++ think-machine-pdf/src/main/java/com/softwaremagico/tm/pdf/complete/PdfDocument.java
@@ -10,7 +10,8 @@ import com.softwaremagico.tm.log.PdfExporterLog;
 import com.softwaremagico.tm.pdf.complete.events.FooterEvent;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /*
  * #%L
@@ -37,92 +38,94 @@ import java.io.FileOutputStream;
  */
 
 public abstract class PdfDocument {
-	private int rightMargin = 30;
-	private int leftMargin = 30;
-	private int topMargin = 30;
-	private int bottomMargin = 30;
-	private final String language;
-	private final String moduleName;
+    private int rightMargin = 30;
+    private int leftMargin = 30;
+    private int topMargin = 30;
+    private int bottomMargin = 30;
+    private final String language;
+    private final String moduleName;
 
-	protected PdfDocument(String language, String moduleName) {
-		this.language = language;
-		this.moduleName = moduleName;
-	}
+    protected PdfDocument(String language, String moduleName) {
+        this.language = language;
+        this.moduleName = moduleName;
+    }
 
-	protected Document addMetaData(Document document) {
-		document.addTitle("Fading Suns Character Sheet");
-		document.addAuthor("Software Magico");
-		document.addCreator("Think Machine");
-		document.addSubject("RPG");
-		document.addKeywords("RPG, Fading Suns, FS, " + language);
-		document.addCreationDate();
-		return document;
-	}
+    protected Document addMetaData(Document document) {
+        document.addTitle("Fading Suns Character Sheet");
+        document.addAuthor("Software Magico");
+        document.addCreator("Think Machine");
+        document.addSubject("RPG");
+        document.addKeywords("RPG, Fading Suns, FS, " + language);
+        document.addCreationDate();
+        return document;
+    }
 
-	private void generatePDF(Document document, PdfWriter writer) throws EmptyPdfBodyException, InvalidXmlElementException, DocumentException {
-		addMetaData(document);
-		document.open();
-		// createCharacterPDF(document);
-		createContent(document);
-		document.close();
-	}
+    private void generatePDF(Document document, PdfWriter writer) throws EmptyPdfBodyException, InvalidXmlElementException, DocumentException {
+        addMetaData(document);
+        document.open();
+        // createCharacterPDF(document);
+        createContent(document);
+        document.close();
+    }
 
-	protected abstract void createContent(Document document) throws InvalidXmlElementException, DocumentException;
+    protected abstract void createContent(Document document) throws InvalidXmlElementException, DocumentException;
 
-	protected void addEvent(PdfWriter writer) {
-		writer.setPageEvent(new FooterEvent());
-	}
+    protected void addEvent(PdfWriter writer) {
+        writer.setPageEvent(new FooterEvent());
+    }
 
-	/**
-	 * Pdf as byte array. Be careful with big PDF files.
-	 * 
-	 * @return
-	 * @throws EmptyPdfBodyException
-	 * @throws DocumentException
-	 * @throws InvalidXmlElementException
-	 */
-	public final byte[] generate() throws EmptyPdfBodyException, DocumentException, InvalidXmlElementException {
-		final Document document = new Document(getPageSize(), rightMargin, leftMargin, topMargin, bottomMargin);
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		final PdfWriter writer = PdfWriter.getInstance(document, baos);
-		addEvent(writer);
-		generatePDF(document, writer);
-		return baos.toByteArray();
+    /**
+     * Pdf as byte array. Be careful with big PDF files.
+     *
+     * @return
+     * @throws EmptyPdfBodyException
+     * @throws DocumentException
+     * @throws InvalidXmlElementException
+     */
+    public final byte[] generate() throws EmptyPdfBodyException, DocumentException, InvalidXmlElementException {
+        final Document document = new Document(getPageSize(), rightMargin, leftMargin, topMargin, bottomMargin);
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final PdfWriter writer = PdfWriter.getInstance(document, baos);
+        addEvent(writer);
+        generatePDF(document, writer);
+        return baos.toByteArray();
 
-	}
+    }
 
-	protected abstract void addDocumentWriterEvents(PdfWriter writer);
+    protected abstract void addDocumentWriterEvents(PdfWriter writer);
 
-	public int createFile(String path) {
-		if (!path.endsWith(".pdf")) {
-			path += ".pdf";
-		}
+    public int createFile(String path) {
+        if (!path.endsWith(".pdf")) {
+            path += ".pdf";
+        }
 
-		// DIN A6 105 x 148 mm
-		final Document document = new Document(getPageSize(), rightMargin, leftMargin, topMargin, bottomMargin);
+        // DIN A6 105 x 148 mm
+        final Document document = new Document(getPageSize(), rightMargin, leftMargin, topMargin, bottomMargin);
 
-		// if (!MyFile.fileExist(path)) {
-		try {
-			final PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(path));
-			addEvent(writer);
-			generatePDF(document, writer);
-			return writer.getPageNumber();
-		} catch (Exception e) {
-			PdfExporterLog.errorMessage(this.getClass().getName(), e);
-			return 0;
-		}
-		// }
-	}
+        // if (!MyFile.fileExist(path)) {
+        try {
+            final PdfWriter writer = PdfWriter.getInstance(document, Files.newOutputStream(Paths.get(path)));
+            addEvent(writer);
+            generatePDF(document, writer);
+            return writer.getPageNumber();
+        } catch (Exception e) {
+            PdfExporterLog.errorMessage(this.getClass().getName(), e);
+            return 0;
+        } finally {
+            document.close();
+        }
+        // }
+    }
 
-	protected abstract Rectangle getPageSize();
+    protected abstract Rectangle getPageSize();
 
-	protected abstract void createCharacterPDF(Document document, CharacterPlayer character) throws Exception;
+    protected abstract void createCharacterPDF(Document document, CharacterPlayer character) throws Exception;
 
-	public String getLanguage() {
-		return language;
-	}
+    public String getLanguage() {
+        return language;
+    }
 
-	public String getModuleName() {
-		return moduleName;
-	}
+    public String getModuleName() {
+        return moduleName;
+    }
 }
